016e3791bf788fd7e38883fc53d7e1c9853260bd
Codepressd <creederone@gmail.com>
2016-07-12 12:27:16 -0700

    created random number script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
-	
+	var randNum = randomNum();
+	var amtGuess = 0;
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -15,6 +17,13 @@ $(document).ready(function(){
   	/* New Game clears random num, clears guesses, generates new random num */
 
   	/* Create a random number between 1 and 100*/
+
+  	function randomNum (){
+
+  		return Math.floor((Math.random() * 100) + 1);
+  		
+
+  	};
 
   	/*submit guess and track how many guesses and how close to the number*/
 
