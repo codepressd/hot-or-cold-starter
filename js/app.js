4571d8d0089d6d030bad2ed6c7fda9902d17f74a
Codepressd <creederone@gmail.com>
2016-07-12 12:05:19 -0700

    wrote out the problems I need to solve

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+  	/* New Game clears random num, clears guesses, generates new random num */
+
+  	/* Create a random number between 1 and 100*/
+
+  	/*submit guess and track how many guesses and how close to the number*/
+
+
+});
+
+
